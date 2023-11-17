bc5e9f0e2de5e2a55ec813a78c9b5b28a96c62ef
Kenny48620 <kennyleetrade@gmail.com>
2023-11-17 00:02:30 -0800

    Dummy file

---
+++ WA8_11/Main Screen/MainFirebaseManager.swift
@@ -0,0 +1,18 @@
+//
+//  MainFirebaseManager.swift
+//  WA8_11
+//
+//  Created by 李凱鈞 on 11/16/23.
+//
+
+import Foundation
+
+extension ViewController{
+    //
+    
+    func checkChatIfExist(){
+        
+    }
+    
+    
+}
