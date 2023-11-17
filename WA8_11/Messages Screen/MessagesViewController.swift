8e4b439cf6dc4276c90f1843536e1501e5af8b14
Kaylin Lau <lau.kay@northeastern.edu>
2023-11-16 23:14:50 -0800

    added message collection and documnet to store email date and message

--- WA8_11/Messages Screen/MessagesViewController.swift
+++ WA8_11/Messages Screen/MessagesViewController.swift
@@ -14,6 +14,8 @@ class MessagesViewController: UIViewController {
     let messagesScreen = MessagesView()
     var receiver: Contact!
     
+    var currentChatID: String?
+    
     // use this listener to track whether any user is signed in.
     var handleAuth: AuthStateDidChangeListenerHandle?
     // a variable to keep an instance of the current signed-in Firebase user.
@@ -27,53 +29,53 @@ class MessagesViewController: UIViewController {
         view = messagesScreen
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-
-        //MARK: handling if the Authentication state is changed (sign in, sign out, register)...
-        handleAuth = Auth.auth().addStateDidChangeListener{ auth, user in
-            if user == nil{
-                //MARK: not signed in.
-                
-            }else{
-                //MARK: the user is signed in.
-                self.currentUser = user
-
-                //MARK: Observe Firestore database to display the contacts list...
-                // all users are friends with other, add all users except the signed in user
-                self.database.collection("chats")
-                    .addSnapshotListener(includeMetadataChanges: false, listener: {querySnapshot, error in
-                        if let documents = querySnapshot?.documents{
-                            self.messagesList.removeAll()
-                            for document in documents{
-                                do{
-                                    // Access the "name" field
-                                    let name = try document.get("name") as? String ?? ""
-                                    // Get the document ID (assuming it's the email)
-                                    let email = document.documentID
-                                    // If the email is same as current user's email
-                                    if email == self.currentUser?.email{
-                                        continue
-                                    }
-
-                                    let message = Message(senderName: sendername, textMessages: text, momentInTime: Date)
-                                    self.messagesList.append(message)
-                                }catch{
-                                    print(error)
-                                }
-                            }
-                            self.contactList.sort(by: {$0.name < $1.name})
-                            self.messagesScreen.tableViewMessages.reloadData()
-                        }
-                    })
-
-
-                //MARK: Observe Firestore database to display the contacts list...
-
-            }
-        }
-    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//
+//        //MARK: handling if the Authentication state is changed (sign in, sign out, register)...
+//        handleAuth = Auth.auth().addStateDidChangeListener{ auth, user in
+//            if user == nil{
+//                //MARK: not signed in.
+//
+//            }else{
+//                //MARK: the user is signed in.
+//                self.currentUser = user
+//
+//                //MARK: Observe Firestore database to display the contacts list...
+//                // all users are friends with other, add all users except the signed in user
+//                self.database.collection("chats")
+//                    .addSnapshotListener(includeMetadataChanges: false, listener: {querySnapshot, error in
+//                        if let documents = querySnapshot?.documents{
+//                            self.messagesList.removeAll()
+//                            for document in documents{
+//                                do{
+//                                    // Access the "name" field
+//                                    let name = try document.get("name") as? String ?? ""
+//                                    // Get the document ID (assuming it's the email)
+//                                    let email = document.documentID
+//                                    // If the email is same as current user's email
+//                                    if email == self.currentUser?.email{
+//                                        continue
+//                                    }
+//
+//                                    let message = Message(senderName: sendername, textMessages: text, momentInTime: Date)
+//                                    self.messagesList.append(message)
+//                                }catch{
+//                                    print(error)
+//                                }
+//                            }
+//                            self.contactList.sort(by: {$0.name < $1.name})
+//                            self.messagesScreen.tableViewMessages.reloadData()
+//                        }
+//                    })
+//
+//
+//                //MARK: Observe Firestore database to display the contacts list...
+//
+//            }
+//        }
+//    }
   
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -87,26 +89,35 @@ class MessagesViewController: UIViewController {
         
         // Check if a chat already exists
         checkForExistingChat(with: receiver, currentUserEmail: currentUserEmail)
+        
+        // Set up button action
+        messagesScreen.buttonPost.addTarget(self, action: #selector(postMessage), for: .touchUpInside)
 
     }
+
     
     private func checkForExistingChat(with receiver: Contact, currentUserEmail: String) {
-            database.collection("users").document(currentUserEmail).collection("chats")
-                .whereField("receiverEmail", isEqualTo: receiver.email)
-                .getDocuments { [weak self] (snapshot, error) in
-                    if let error = error {
-                        print("Error checking for existing chat: \(error)")
-                        return
-                    }
-
-                    if let documents = snapshot?.documents, documents.isEmpty {
-                        // No existing chat, create a new one
-                        self?.createChat(with: receiver, currentUserEmail: currentUserEmail)
-                    } else {
-                        print("Chat already exists with \(receiver.name)")
-                    }
+        database.collection("users").document(currentUserEmail).collection("chats")
+            .whereField("receiverEmail", isEqualTo: receiver.email)
+            .getDocuments { [weak self] (snapshot, error) in
+                if let error = error {
+                    print("Error checking for existing chat: \(error)")
+                    return
                 }
-        }
+
+                if let documents = snapshot?.documents, !documents.isEmpty {
+                    // Existing chat found, set the currentChatID
+                    if let chatDocument = documents.first {
+                        self?.currentChatID = chatDocument.documentID
+                        print("Existing chat ID set: \(String(describing: self?.currentChatID))")
+                    }
+                } else {
+                    // No existing chat, create a new one
+                    self?.createChat(with: receiver, currentUserEmail: currentUserEmail)
+                }
+            }
+    }
+
 
     private func createChat(with receiver: Contact, currentUserEmail: String) {
         let newChatID = database.collection("users").document(currentUserEmail).collection("chats").document().documentID
@@ -136,7 +147,58 @@ class MessagesViewController: UIViewController {
         
         // Create the actual chat document in the 'chats' collection
         database.collection("chats").document(newChatID).setData(["participants": [currentUserEmail, receiver.email]])
+        
+        self.currentChatID = newChatID
+        print("I am here", self.currentChatID)
     }
+    
+    
+    @objc private func postMessage() {
+        
+        print("Post Message function")
+
+            // Print the text from the textView
+            print("Text from textView: \(String(describing: messagesScreen.textViewNote.text))")
+
+            // Check if the message text is empty
+            guard let messageText = messagesScreen.textViewNote.text, !messageText.isEmpty else {
+                print("Message text is empty")
+                return
+            }
+
+            // Check if the currentChatID is set
+        guard let chatID = self.currentChatID else {
+                print("Current chat ID is nil")
+                return
+            }
+            print("Current Chat ID: \(chatID)")
+
+            // Check if the currentUser's email is available
+            guard let currentUserEmail = Auth.auth().currentUser?.email else {
+                print("Current user's email is nil")
+                return
+            }
+            print("Sender Email: \(currentUserEmail)")
+//        guard let messageText = messagesScreen.textViewNote.text, !messageText.isEmpty,
+//              let chatID = self.currentChatID,
+//              let senderEmail = currentUser?.email else {
+//            print("No text or chat ID or current user email")
+//            return
+//        }
+
+        let newMessage = ["text": messageText, "senderEmail": currentUserEmail, "timestamp": FieldValue.serverTimestamp()] as [String : Any]
+        let messageDocument = database.collection("chats").document(chatID).collection("messages").document()
+
+        messageDocument.setData(newMessage) { error in
+            if let error = error {
+                print("Error saving message: \(error)")
+            } else {
+                print("Message saved successfully")
+                self.messagesScreen.textViewNote.text = "" // Clear text field after sending
+            }
+        }
+    }
+    
     
 //    // another lifecycle method where you can handle the logic right before the screen disappears.
 //    override func viewWillDisappear(_ animated: Bool) {
